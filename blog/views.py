26f741946a1c78928c1843451113bef17bda49c9
hasan <hasanmirzaie57@gmail.com>
2025-05-19 12:09:27 +0330

    paginated fixed from template

--- blog/views.py
+++ blog/views.py
@@ -16,15 +16,11 @@ def about(request):
     return render(request, "about.html")
 
 
-# def contact(request):
-#     return render(request, "contact.html")
-
-
 class BlogListView(ListView):
     model = Post
     template_name = "blog/blog-list.html"
     context_object_name = "posts"
-    paginate_by = 10  # اضافه کردن pagination
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
