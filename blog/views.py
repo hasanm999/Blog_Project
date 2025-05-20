4ed7e2ab2d2dbeedeab1fff1e2dd60f9d464f1f0
hasan <hasanmirzaie57@gmail.com>
2025-05-20 15:09:57 +0330

    about post author added to blog detail

--- blog/views.py
+++ blog/views.py
@@ -65,7 +65,8 @@ class BlogDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
+        context['categories'] = Category.objects.annotate(post_count=Count('post')).filter(post_count__gt=0)
+        context['tags'] = Tag.objects.all()
         return context
 
 
